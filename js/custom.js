5fa9eae85b0574fdf4794b421ec8849a16275417
agazademurvet <murvetagazade@gmail.com>
2022-11-25 08:16:52 -0500

    hazir

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1500); 
+    });
+
+    // CUSTOM LINK
+  
+    
+  })(window.jQuery);
+
+
